5f850991af6fd46825afb5113461c3552ff1aaa1
kay-ai <favourobasi6@gmail.com>
2020-07-15 14:52:03 +0100

    new

--- getemail.php
+++ getemail.php
@@ -6,15 +6,23 @@ $password = "20e8af9269f5936a79573c106e567ce2f7bc7ebcd1fee2f4fe2f2d98f140681b";
 $dbname = "d2hvol779nos4o";
 $port = "5432";
 
-// Create connection
-$conn = pg_connect("host=$host user=$user password=$password dbname=$dbname");;
+try{
+  // Create connection
+  $dsn = "pgsql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname . ";user=" . $user . ";password=" . $password;
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+$pdo = new PDO($dsn, $user, $password);
 
-function redirect_to( $location = NULL, $email) {
+// display a message if connected to database successfully
+if($pdo){
+  echo "Connected to the <strong>$db</strong> database successfully!";
+         }
+ }catch (PDOException $e){
+  // report error message
+  echo $e->getMessage();
+ }
+
+/* function redirect_to( $location = NULL, $email) {
   if ($location != NULL) {
       header("Location: $location?message=success&email=$email");
       exit;
@@ -24,12 +32,13 @@ function redirect_to( $location = NULL, $email) {
 $email = $_POST['email'];
 
 //Insert Values
-$result = pg_query($conn, "INSERT INTO emails (email) VALUES ('$email')");
+$sql = "INSERT INTO emails (email)
+VALUES ('$email')";
 
-if ($result === TRUE) {
+if ($conn->query($sql) === TRUE) {
     $message = 'success';
     redirect_to('index.php', $email);
 } else {
-  echo "Error: " . $result . "<br>" . $conn->error;
-}
+  echo "Error: " . $sql . "<br>" . $conn->error;
+} */
 ?>
