515fc4e2659e033d73fa50249996033858be6043
kay-ai <favourobasi6@gmail.com>
2020-07-15 15:38:55 +0100

    qq

--- getemail.php
+++ getemail.php
@@ -6,23 +6,9 @@ $password = "bd4778af29f0f3c4fdee4d83d9d06b5432b146c60b017365137618e76389ae3d";
 $dbname = "db9eso0pclbukt";
 $port = "5432";
 
-try{
-  //Set DSN data source name
-    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
-
-  //create a pdo instance
-  $pdo = new PDO($dsn, $user, $password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-echo 'Connection failed: ' . $e->getMessage();
-}
-
-/* // Create connection
-$conn = pg_connect("host=$host user=$user password=$password");
+// Create connection
+$conn = pg_connect("host=$host user=$user password=$password dbname=$dbname");
 
 // Check connection
 if ($conn) {
@@ -46,11 +32,12 @@ $email = $_POST['email'];
 $sql = "INSERT INTO emails(email) VALUES ('$email')";
 $result = pg_exec($conn, $sql);
 
-if ($result) {
-    $message = 'success';
-    redirect_to('index.php', $email);
-} else {
-  echo "Error: " . $sql . pg_errormessage($conn);
-}
-pg_close($conn); */
+  if ($result) {
+      $message = 'success';
+      redirect_to('index.php', $email);
+  } else {
+    echo "Error: " . $sql . pg_errormessage($conn);
+  }
+
+pg_close($conn);
 ?>
